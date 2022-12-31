481a8a6cea2f625700734f18cc345d32602f5bf1
= <=>
2023-01-01 01:21:26 +0700

    build

--- build/_app/immutable/chunks/3-7688f018.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-c0a4e4d0.js";export{t as component};
---
+++ build/_app/immutable/chunks/roblox-2ece0dea.js
@@ -0,0 +1,1 @@
+const s=""+new URL("../assets/discord-04f25cdb.svg",import.meta.url).href,o=""+new URL("../assets/roblox-a6f55b9b.svg",import.meta.url).href;export{s as D,o as R};
