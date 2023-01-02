77e5428bb42526cdbe89c5668ca198b625e11cfd
CutieRei <cutierei.py@gmail.com>
2023-01-02 20:23:47 +0700

    build

---
+++ build/_app/immutable/chunks/0-57400163.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-da46b06b.js";import{default as t}from"../components/pages/_layout.svelte-bde7ae25.js";export{t as component,r as universal};
--- build/_app/immutable/chunks/2-261b6987.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-268c7406.js";export{t as component};
